2247725b59d4f1c517ba4c2b702fb011a5b8c149
RandyWiratamaa <randywiratama1@gmail.com>
2023-01-25 14:50:39 +0700

    Export Postman

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -21,8 +21,16 @@
         </style>
     </head>
     <body class="antialiased">
-        <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
-            <h1>Restful API Product Management V1.0.0</h1>
+        <div class="relative flex items-top justify-center min-h-screen sm:items-center py-4 sm:pt-0">
+            <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
+                <div class="flex justify-center mt-4 sm:items-center sm:justify-between">
+                    <div class="text-center text-sm text-gray-500 sm:text-left">
+                        <div class="flex items-center">
+                            Restful API Product Management V1.0.0 <br/>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </body>
 </html>
